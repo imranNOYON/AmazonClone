a1168ed09a3a883c0fb5d62b4908f96f5383b19d
Md Imran Hosen <noyon.imran555@gmail.com>
2025-03-21 20:59:29 +0600

    new

--- src/app/components/AddToCart.tsx
+++ src/app/components/AddToCart.tsx
@@ -11,9 +11,10 @@ interface Props {
 const AddToCart = ({ product, className }: Props) => {
   return (
     <button
-       onClick={()=>toast.success("Added Successfully")}
+    onClick={() => toast.success(`${product.title} added to cart`)}
       className={twMerge(
-        "text-sm tracking-wide font-medium mb-2 border-[1px] border-colors-amazonBlue/50 py-2 rounded-full bg-colors-amazonLight hover:bg-colors-amazonYellowDark duration-300",className
+        "text-sm tracking-wide font-medium mb-2 border-[1px] border-colors-amazonBlue/50 py-2 rounded-full bg-colors-amazonLight hover:bg-colors-amazonYellowDark duration-300",
+        className
       )}
     >
       Add to cart
--- src/app/components/FooterMiddleList.tsx
+++ src/app/components/FooterMiddleList.tsx
@@ -1,16 +1,22 @@
+interface ListData {
+  listData: string[];
+}
+
 interface Props {
   title: string;
-  listItem: any;
+  listItem: ListData[];
 }
 
 const FooterMiddleList = ({ title, listItem }: Props) => {
   return (
     <div>
-      <h3 className="font-semibold text-white text-base mb-3">{title} </h3>
+      <h3 className="font-semibold text-white text-base mb-3">{title}</h3>
       <ul className="flex flex-col gap-0.5">
-        {listItem?.map((item: any) =>
-          item?.listData.map((data: string) => (
-            <li className="footerLink">{data}</li>
+        {listItem?.map((item, itemIndex) =>
+          item?.listData.map((data: string, dataIndex) => (
+            <li key={`${itemIndex}-${dataIndex}`} className="footerLink">
+              {data}
+            </li>
           ))
         )}
       </ul>
